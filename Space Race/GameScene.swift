ffd6d0a7f414aeda3564ac597c576ba6b28b19fb
ArturAzarau <artur.azarau@gmail.com>
2018-08-12 11:38:16 +0300

    - Background, player and scoreLabel created

--- Space Race/GameScene.swift
+++ Space Race/GameScene.swift
@@ -11,12 +11,58 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    // MARK: - Properties
+    private var starfield: SKEmitterNode!
+    private var player: SKSpriteNode!
+    private var scoreLabel: SKLabelNode!
+    private var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+
+    // MARK: - Scene life cycle
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        createBackground()
+        createPlayer()
+        createScoreLabel()
     }
     
+    // MARK: - Handling touches
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
+    
+    // MARK: - Methods
+    private func createBackground() {
+        let starfield = SKEmitterNode(fileNamed: "starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+    }
+    
+    private func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+    }
+    
+    private func createScoreLabel() {
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .center
+        addChild(scoreLabel)
+    }
+    
+    private func configurePhysicsWorld() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+    
 }
